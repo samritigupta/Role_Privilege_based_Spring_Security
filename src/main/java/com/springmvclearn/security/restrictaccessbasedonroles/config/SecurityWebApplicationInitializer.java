371d766fe1ddbcccfd1d088e1a43b82927c2aed2
samritigupta <gupta.samriti1991@gmail.com>
2020-12-23 01:05:56 +0000

    role based authorisation

---
+++ src/main/java/com/springmvclearn/security/restrictaccessbasedonroles/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.springmvclearn.security.restrictaccessbasedonroles.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
